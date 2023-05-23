5167eb4f4e8b6e1a3c9e0d42329adc2e4baed42b
EVANGELINE MONICA A/P RAJEN <93172320+monica-2213@users.noreply.github.com>
2023-05-23 11:32:25 +0800

    Update NER.py

--- NER.py
+++ NER.py
@@ -1,56 +1,43 @@
 import streamlit as st
 import pandas as pd
-import spacy
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-# Load the dataset
-data = pd.read_csv('food_nutrition_dataset.csv')
+# Load and preprocess the dataset
+df = pd.read_csv('menu.csv')  # Replace 'menu.csv' with the path to your dataset
+df = df.drop_duplicates(subset='menu_id')  # Remove duplicates
+df = df.dropna(subset=['description', 'ingredients'])  # Remove rows with missing values
 
-# Load the spacy model
-nlp = spacy.load("en_core_web_sm")
+# Initialize and fit the TF-IDF vectorizer
+vectorizer = TfidfVectorizer(stop_words='english')
+tfidf_matrix = vectorizer.fit_transform(df['description'] + ' ' + df['ingredients'])
 
-# Function to detect allergens in the ingredient list using NER
-def detect_allergens_ner(ingredient_list):
-    doc = nlp(ingredient_list)
-    allergens = []
-    for ent in doc.ents:
-        if ent.label_ == 'ALLERGEN':
-            allergens.append(ent.text)
-    return allergens
+# Function to recommend menu items based on user preferences
+def recommend_menu_items(user_preferences, top_n=5):
+    # Transform user preferences into a TF-IDF vector
+    user_preferences_vector = vectorizer.transform([user_preferences])
 
-# Function to detect allergens in the ingredient list using word embeddings
-def detect_allergens_embeddings(ingredient_list, allergens):
-    detected_allergens = []
-    for allergen in allergens:
-        if allergen.lower() in ingredient_list.lower():
-            detected_allergens.append(allergen)
-    return detected_allergens
+    # Calculate cosine similarity between user preferences and all menu items
+    similarity_scores = cosine_similarity(user_preferences_vector, tfidf_matrix).flatten()
 
-# Streamlit app
+    # Sort menu items based on similarity scores and get top recommendations
+    top_indices = similarity_scores.argsort()[::-1][:top_n]
+    recommendations = df.iloc[top_indices]['menu_item']
+
+    return recommendations
+
+# Streamlit app code
 def main():
-    st.title("Allergen Detection App")
-    st.write("Enter a list of ingredients and check for potential allergens.")
+    # App title
+    st.title('Menu Recommendation')
 
-    # Get user input
-    ingredient_list = st.text_area("Enter the list of ingredients (separated by commas)")
-
-    if st.button("Detect Allergens (NER)"):
-        detected_allergens_ner = detect_allergens_ner(ingredient_list)
-        if detected_allergens_ner:
-            st.warning("Potential allergens detected (NER):")
-            for allergen in detected_allergens_ner:
-                st.write("- " + allergen)
-        else:
-            st.success("No potential allergens detected using NER.")
-
-    if st.button("Detect Allergens (Word Embeddings)"):
-        allergens = ['peanuts', 'milk', 'eggs', 'soy', 'wheat', 'tree nuts', 'fish', 'shellfish']
-        detected_allergens_embeddings = detect_allergens_embeddings(ingredient_list, allergens)
-        if detected_allergens_embeddings:
-            st.warning("Potential allergens detected (Word Embeddings):")
-            for allergen in detected_allergens_embeddings:
-                st.write("- " + allergen)
-        else:
-            st.success("No potential allergens detected using Word Embeddings.")
+    # User input section
+    user_preferences = st.text_area('Enter your preferences', height=100)
+    if st.button('Recommend'):
+        recommendations = recommend_menu_items(user_preferences)
+        st.subheader('Recommended Menu Items:')
+        for i, item in enumerate(recommendations):
+            st.write(f"{i+1}. {item}")
 
 if __name__ == '__main__':
     main()
